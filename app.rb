794cdf404fc7db693c8f5d31d693772445cbe58d
FelixCao-5727 <yc7413@nyu.edu>
2026-09-26 00:51:42 -0400

    Part 4 complete

--- app.rb
+++ app.rb
@@ -39,8 +39,14 @@ class WordGuesserApp < Sinatra::Base
   # If a guess is repeated, set flash[:message] to "You have already used that letter."
   # If a guess is invalid, set flash[:message] to "Invalid guess."
   post '/guess' do
-    params[:guess].to_s[0]
+    letter=params[:guess].to_s[0]
     ### YOUR CODE HERE ###
+    begin
+      result = @game.guess(letter)
+      flash[:message] ='You have already used that letter.' if result == false
+    rescue ArgumentError => e
+      flash[:messsage] = e.message
+    end
     redirect '/show'
   end
 
@@ -51,16 +57,22 @@ class WordGuesserApp < Sinatra::Base
   # wrong_guesses and word_with_guesses from @game.
   get '/show' do
     ### YOUR CODE HERE ###
+    case @game.check_win_or_lose
+    when :win then redirect '/win'
+    when :lose then redirect '/lose'
+    end
     erb :show # You may change/remove this line
   end
 
   get '/win' do
     ### YOUR CODE HERE ###
+    redirect '/show' unless @game.check_win_or_lose == :win
     erb :win # You may change/remove this line
   end
 
   get '/lose' do
     ### YOUR CODE HERE ###
+    redirect '/show' unless @game.check_win_or_lose == :lose
     erb :lose # You may change/remove this line
   end
 end
