c9b0355a990bcfdf359e7766541ebea6d522c315
FelixCao-5727 <yc7413@nyu.edu>
2026-09-26 00:04:00 -0400

    Ready for Heroku

--- lib/wordguesser_game.rb
+++ lib/wordguesser_game.rb
@@ -3,9 +3,12 @@ class WordGuesserGame
   # to make the tests in spec/wordguesser_game_spec.rb pass.
 
   # Get a word from remote "random word" service
+  attr_accessor :word, :guesses, :wrong_guesses
 
   def initialize(word)
     @word = word
+    @guesses=''
+    @wrong_guesses=''
   end
 
   # You can test it by installing irb via $ gem install irb
@@ -18,6 +21,31 @@ class WordGuesserGame
     uri = URI('https://esaas-randomword-27a759b6224d.herokuapp.com/RandomWord') 
     Net::HTTP.start(uri.host, uri.port, use_ssl: true) do |http| 
       return http.post(uri, "").body
+    end
+  end
+  def guess(letter)
+    raise ArgumentError, 'Invalid guess.' if letter.nil? || letter.empty? || letter !~ /\A[a-zA-Z]\z/
+    letter=letter.downcase
+    if @guesses.include?(letter) || @wrong_guesses.include?(letter)
+      return false
+    end
+    if @word.downcase.include?(letter)
+      @guesses << letter
+    else
+      @wrong_guesses << letter
+    end
+    true
+  end
+  def word_with_guesses
+    @word.chars.map { |c| @guesses.include?(c.downcase) ? c : '-'}.join
+  end
+  def check_win_or_lose
+    if @word.chars.all? { |c| @guesses.include?(c.downcase) }
+      :win
+    elsif @wrong_guesses.length >= 7
+      :lose
+    else
+      :play
     end
   end
 end
